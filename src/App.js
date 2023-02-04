f48b15d23e2628367c8e47a210e8b3e3585baa85
naoto24kawa <s1210202@gmail.com>
2023-02-04 11:51:07 +0900

    station8まで解決

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import {useState} from 'react'
 import './App.css'
 
 /**
@@ -8,9 +9,20 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg")
+
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src={dogUrl} />
+      <br />
+      <button onClick={() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then((data) => setDogUrl(data.message))
+      }}>更新</button>
     </div>
   )
 }
